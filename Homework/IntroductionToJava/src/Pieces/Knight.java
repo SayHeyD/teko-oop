74b77a4d780eeea69f9d88e94b45e8648b22afec
David Docampo <david@opsone.ch>
2026-04-27 15:43:54 +0200

    feat: implement knight rules

--- Homework/IntroductionToJava/src/Pieces/Knight.java
+++ Homework/IntroductionToJava/src/Pieces/Knight.java
@@ -1,6 +1,9 @@
 package Pieces;
 
+import Board.Chessboard;
 import Board.Field;
+
+import java.util.ArrayList;
 
 public class Knight extends Piece {
     public Knight(Color color) {
@@ -9,12 +12,67 @@ public class Knight extends Piece {
     }
 
     @Override
-    public boolean move(Field field) {
-        return false;
-    }
-
-    @Override
     public Field[] getMoves() {
-        return new Field[0];
+        ArrayList<Field> possibleMoves = new ArrayList<>();
+
+        // Define all possible moves for the knight
+        int[][] possibleMovesCoordinates = new int[][]{
+                {
+                    getField().getCoordinates()[0] + 2,
+                    getField().getCoordinates()[1] + 1
+                },
+
+                {
+                    getField().getCoordinates()[0] + 2,
+                    getField().getCoordinates()[1] - 1
+                },
+
+                {
+                    getField().getCoordinates()[0] + 1,
+                    getField().getCoordinates()[1] + 2
+                },
+
+                {
+                    getField().getCoordinates()[0] - 1,
+                    getField().getCoordinates()[1] + 2
+                },
+
+                {
+                    getField().getCoordinates()[0] - 2,
+                    getField().getCoordinates()[1] + 1
+                },
+
+                {
+                    getField().getCoordinates()[0] - 2,
+                    getField().getCoordinates()[1] - 1
+                },
+
+                {
+                    getField().getCoordinates()[0] + 1,
+                    getField().getCoordinates()[1] - 2
+                },
+
+                {
+                    getField().getCoordinates()[0] - 1,
+                    getField().getCoordinates()[1] - 2
+                },
+        };
+
+        for (int[] coordinates : possibleMovesCoordinates) {
+            Field field = Chessboard.getField(coordinates);
+            // Check if the field exists
+            if (field == null) {
+                continue;
+            }
+
+            // Check if the field is not empty and contains an ally
+            if (field.getPiece() != null && field.getPiece().getColor() == getColor()) {
+                continue;
+            }
+
+            possibleMoves.add(field);
+        }
+
+        return possibleMoves.toArray(new Field[0]);
     }
 }
